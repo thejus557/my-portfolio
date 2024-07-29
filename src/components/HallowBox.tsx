bd624aa70f18818ffa5ace2ea903d57716c90399
Surya <suryateja.kandukuru@techolution.com>
2024-07-29 13:34:37 +0530

    fix: styles for hallow box

--- src/components/HallowBox.tsx
+++ src/components/HallowBox.tsx
@@ -14,10 +14,20 @@ const HallowBox = ({ image, skillName }: HallowBoxProps) => {
         width: "100px",
         borderRadius: "1rem",
         padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-between",
       }}
     >
       <img src={image} alt={skillName} className="w-6 h-6 md:h-12 md:w-12" />
-      <Typography sx={{ color: "text.secondary" }}>{skillName}</Typography>
+      <Typography
+        sx={{
+          color: "text.secondary",
+        }}
+      >
+        {skillName}
+      </Typography>
     </Box>
   );
 };
